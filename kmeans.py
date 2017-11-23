1170ed16d0af93d09f9053144b6a73d43104d590
mohamedpsc <mohamedpsc@gmail.com>
2017-11-23 12:26:30 +0200

    assignments can now be represented by image

--- kmeans.py
+++ kmeans.py
@@ -28,14 +28,15 @@ def compute_new_centroids(data, assignments):
     :type data: numpy matrix, shape(n_samples, n_features)
     :param assignments: determine cluster for each samples.
     :type assignments: numpy matrix, shape(n_samples, 1)
-    :param k: number of clusters (centroids)
-    :type k: int
     :return: new centroids
     :rtype: numpy matrix, shape(n_clusters, n_features)
     """
+    # Sorting data samples such that samples belongs to the same cluster are grouped together.
     order = np.argsort(assignments)
     data = data[order]
+    # calculating number of samples per cluster
     clusters, spc = np.unique(assignments, return_counts=True)
+    # computing new centroids
     centroids = np.zeros((len(clusters), data.shape[1]))
     start = 0
     for i, x in enumerate(spc):
@@ -59,6 +60,27 @@ def compute_error(new_centroids, centroids):
     for x, y in zip(new_centroids, centroids):
         sum_err += np.linalg.norm(x - y)
     return sum_err
+
+
+def draw_clusters(assignments, k, shape):
+    """
+    draw data samples as image of colored clusters
+
+    :param assignments: define assigned cluster per each sample
+    :type assignments: numpy matrix, shape(n_samples, 1)
+    :param k: number of clusters
+    ;:type k: int
+    :param shape: define the dimension of image needed to represent colored samples
+    ;:type shape: tuple(n_rows, m_columns)
+    :return:
+    """
+    colors = [[i, j, l] for i, j, l in zip(sample(range(0, 255, 5), k), sample(range(0, 255, 5), k),
+                                           sample(range(0, 255, 5), k))]
+    image = np.zeros((shape[0], shape[1], shape[2]))
+    for m in range(shape[0]):
+        for n in range(shape[1]):
+            image[m, n] = colors[assignments[m * shape[1] + n]]
+    return image
 
 
 def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distances, centroids=None):
@@ -86,8 +108,9 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
         if centroids.shape != (k, data.shape[1]):
             raise Exception("Error, Number of centroids given is not the same as number of clusters specified.")
     assignments = None
+    sum_err = None
     for i in range(max_iters):
-        # Calculate Distances
+        # calculate distances
         distances = distance_func(data, centroids)
         # Assigning samples to clusters
         assignments = np.argmin(distances, axis=1)
@@ -96,13 +119,17 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
         # computing error
         sum_err = compute_error(new_centroids, centroids)
         if sum_err < threshold:
-            return new_centroids, assignments
+            return new_centroids, assignments, sum_err
         centroids = new_centroids
-    return centroids, assignments
+    return centroids, assignments, sum_err
 
 
 if __name__ == '__main__':
-    from scipy.misc import imread
-    image = imread('BSR/bench/data/images/2018.jpg')
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
-    kmeans(np.asmatrix(image), 0.00001, 4)
+    from scipy.misc import imread, imsave
+    train_image = imread('BSR/bench/data/images/8068.jpg')
+    image_shape = train_image.shape
+    train_image = train_image.reshape((train_image.shape[0] * train_image.shape[1], 3))
+    k_clusters = 5
+    centers, assigns, error = kmeans(np.asmatrix(train_image), 0.00001, k_clusters)
+    image2 = draw_clusters(assigns, k_clusters, image_shape)
+    imsave('test.png', image2)
