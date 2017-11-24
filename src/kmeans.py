50c94cf5ad0f9c1b4efe1c42f596bd9eaf254ec4
marwan <mrwanbaghdad76@gmail.com>
2017-11-24 23:44:42 +0200

    integeration resource reader in the module's run test'

--- src/kmeans.py
+++ src/kmeans.py
@@ -111,6 +111,7 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
     sum_err = None
     for i in range(max_iters):
         # calculate distances
+        # print("Helo")
         distances = distance_func(data, centroids)
         # Assigning samples to clusters
         assignments = np.argmin(distances, axis=1)
@@ -125,11 +126,15 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
 
 
 if __name__ == '__main__':
-    from scipy.misc import imread, imsave
-    train_image = imread('BSR/bench/data/images/8068.jpg')
+# if True:
+    from scipy.misc import imread, imsave,imshow
+    import resource_reader as rr 
+    # train_image = imread('../BSR/bench/data/images/8068.jpg')
+    train_image = next(rr.request_data())[0]
     image_shape = train_image.shape
     train_image = train_image.reshape((train_image.shape[0] * train_image.shape[1], 3))
     k_clusters = 5
     centers, assigns, error = kmeans(np.asmatrix(train_image), 0.00001, k_clusters)
     image2 = draw_clusters(assigns, k_clusters, image_shape)
-    imsave('test.png', image2)
+    # imsave('test.png', image2)
+    imshow(image2)
