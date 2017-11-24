d1dadf56151bf8f035cf6415ae44faa734783893
marwan <mrwanbaghdad76@gmail.com>
2017-11-24 23:37:08 +0200

    returning a tuple of segmentation and boundaries ndarray not flattened

--- src/misc.py
+++ src/misc.py
@@ -1,10 +1,15 @@
 import numpy as np 
 from os import path 
 from scipy.io import loadmat
+from scipy.misc import imresize
 
 def handle_mat_struct(matobject):
-    SEGMENTATION = 'SEGMENTATION'
-    BOUNDARIES = 'BOUNDARIES'
+    SEGMENTATION = 'Segmentation'
+    BOUNDARIES = 'Boundaries'
+    
     for i in matobject['groundTruth'][0]:
-        yield i[0]
+        yield (
+            i[0][SEGMENTATION][0],
+            i[0][BOUNDARIES][0]
+            )
 
