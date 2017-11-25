e6ebbe5a416c79f14e55f1eb881554d086b63198
mohamedpsc <mohamedpsc@gmail.com>
2017-11-25 10:22:50 +0200

    compute cost for kmeans is now added, evaluate kmeans function is implemented, updated documentation, user can choose colors to represent each cluster in draw_clusters function

--- src/kmeans.py
+++ src/kmeans.py
@@ -62,7 +62,7 @@ def compute_error(new_centroids, centroids):
     return sum_err
 
 
-def draw_clusters(assignments, k, shape):
+def draw_clusters(assignments, k, shape, colors=None):
     """
     draw data samples as image of colored clusters
 
@@ -72,15 +72,45 @@ def draw_clusters(assignments, k, shape):
     ;:type k: int
     :param shape: define the dimension of image needed to represent colored samples
     ;:type shape: tuple(n_rows, m_columns)
-    :return:
+    :param colors: colors to be used to represent each cluster samples
+    :type colors: iterable like (list, nd-array, ..), shape(k, 3)
+    :return: cluster samples drawn in image, such that each cluster samples represented by a different color.
+    :rtype: np.matrix
     """
-    colors = [[i, j, l] for i, j, l in zip(sample(range(0, 255, 5), k), sample(range(0, 255, 5), k),
-                                           sample(range(0, 255, 5), k))]
+    if colors is None:
+        rang = range(0, 255, int(255 / k))
+        colors = [[i, j, l] for i, j, l in zip(sample(rang, k), sample(rang, k), sample(rang, k))]
     image = np.zeros((shape[0], shape[1], shape[2]))
     for m in range(shape[0]):
         for n in range(shape[1]):
             image[m, n] = colors[assignments[m * shape[1] + n]]
     return image
+
+
+def compute_cost(data, assignments, centroids):
+    """
+    compute cost function for the given data, assignments and centroids.
+    cost is the sum of the difference between each data sample and cluster centroid it belongs to
+
+    :param data: data samples
+    :type data: numpy matrix, shape(n_samples, n_features)
+    :param assignments: determine cluster for each samples.
+    :type assignments: numpy matrix, shape(n_samples, 1)
+    :param centroids: clusters representatives
+    :type centroids: numpy matrix, shape(k_clusters, n_features)
+    :return:
+    """
+    # Sorting data samples such that samples belong to the same cluster are grouped together.
+    order = np.argsort(assignments)
+    data = data[order]
+    # calculating number of samples per cluster
+    clusters, spc = np.unique(assignments, return_counts=True)
+    # computing cost
+    cost = start = 0
+    for i, x in enumerate(spc):
+        cost += np.sum(euclidean_distances(data[start: start + x], centroids[i]))
+        start += x
+    return cost / centroids.shape[0]
 
 
 def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distances, centroids=None):
@@ -89,17 +119,18 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
 
     :param data: data samples.
     :type data: numpy matrix, shape(n_samples, n_features)
-    :param threshold: max error ratio allowed.
+    :param threshold: error ratio allowed.
     :type threshold: float, (optional)
     :param k: number of clusters
     :type k: int
     :param max_iters: maximum number of iterations
     :type max_iters: int, (optional)
-    :param distance_func: function used to compute the
+    :param distance_func: function used to compute distance (dissimilarity) between samples and centroids
+    :type distance_func: function, (optional)
     :param centroids: k initial centroids
-    :type centroids: numpy matrix, (optional), shape(k_samples, n_features)
+    :type centroids: numpy matrix, (optional), shape(k_clusters, n_features)
     :return: centroids, samples assignments
-    :rtype: numpy matrix, shape(n_clusters, n_features), numpy matrix, shape(n_samples, 1)
+    :rtype: numpy matrix, shape(k_clusters, n_features), numpy matrix, shape(n_samples, 1)
     """
     if centroids is None:
         # selecting Random centroids from the given data
@@ -108,7 +139,6 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
         if centroids.shape != (k, data.shape[1]):
             raise Exception("Error, Number of centroids given is not the same as number of clusters specified.")
     assignments = None
-    sum_err = None
     for i in range(max_iters):
         # calculate distances
         distances = distance_func(data, centroids)
@@ -118,18 +148,50 @@ def kmeans(data, threshold, k=2, max_iters=100, distance_func=euclidean_distance
         new_centroids = compute_new_centroids(data, assignments)
         # computing error
         sum_err = compute_error(new_centroids, centroids)
+        # min error ratio specified is reached
         if sum_err < threshold:
-            return new_centroids, assignments, sum_err
+            break
         centroids = new_centroids
-    return centroids, assignments, sum_err
+    return centroids, assignments
+
+
+def evaluate_kmeans(data, num_eval, threshold, k=2, max_iters=100, distance_func=euclidean_distances, centroids=None):
+    """
+    Run Kmeans clustering on data multiple times(num_eval) and report result with min cost
+
+    :param data: data samples
+    :param num_eval: number of times to run kmeans on the given data
+    :param threshold: error ratio used by kmeans
+    :param k: number of clusters used by kmeans
+    :param max_iters: max number of iteration used by kmeans
+    :param distance_func: function used by kmeans to compute distance (dissimilarity) between samples and centroids
+    :type distance_func: function, (optional)
+    :param centroids: k initial centroids used by kmeans.
+    :type centroids: numpy matrix, (optional), shape(k_samples, n_features)
+    :return: centroids, samples assignments corresponding to the least cost
+    :rtype: numpy matrix, shape(k_clusters, n_features), numpy matrix, shape(n_samples, 1)
+    """
+    cost = 0
+    best_centroids = best_assignments = None
+    for i in range(num_eval):
+        # computing kmeans on the given data samples
+        centroids, assignments = kmeans(data, threshold, k, max_iters, distance_func, centroids)
+        # computing cost function
+        temp_cost = compute_cost(data, assignments, centroids)
+        if temp_cost < cost or i == 0:
+            cost = temp_cost
+            best_assignments = assignments
+            best_centroids = centroids
+    return best_centroids, best_assignments
 
 
 if __name__ == '__main__':
     from scipy.misc import imread, imsave
-    train_image = imread('BSR/bench/data/images/8068.jpg')
+    train_image = imread('BSR/BSDS500/data/images/train/16052.jpg')
     image_shape = train_image.shape
     train_image = train_image.reshape((train_image.shape[0] * train_image.shape[1], 3))
     k_clusters = 5
-    centers, assigns, error = kmeans(np.asmatrix(train_image), 0.00001, k_clusters)
-    image2 = draw_clusters(assigns, k_clusters, image_shape)
+    best = evaluate_kmeans(train_image, 5, 0.0001, k_clusters)
+    image2 = draw_clusters(best[1], k_clusters, image_shape, best[0])
     imsave('test.png', image2)
+
