b8057868b1d3edbd2d70d2d8eed4dddd8fa4c74f
mohamedpsc <mohamedpsc@gmail.com>
2017-12-03 20:18:10 +0200

    spectral clustering evaluation method modified, evaluation is much faster

--- src/cluster_eval.py
+++ src/cluster_eval.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import resource_reader as reader
-from kmeans import kmeans
-from spectral_clustering import spectral_clustering, rbf, knn
-from eval import fmeasure, conditional_entropy
-=======
 import src.resource_reader as reader
 from src.kmeans import kmeans, draw_clusters
-from src.spectral_clustering import spectral_clustering, rbf, knn
+from src.spectral_clustering import _spectral_clustering, rbf, knn, normalize
 from src.eval import fmeasure, conditional_entropy
->>>>>>> bde5327d1297ce2558ae5b1520d7595fecf87902
 import numpy as np
 from os import path, makedirs, environ
-from itertools import islice
+from itertools import islice, tee
 from scipy.misc import imshow, imread, imresize
 
 KMEANS_DIR = "../kmeans_eval"
@@ -61,8 +54,8 @@ def _evaluate_spectral(dir, data, ground_truth, name, resolution, k_clusters, si
     """
     Apply Spectral Clustering on given data then evaluate f-measure and conditional entropy for given ground truths.
 
-    :param dir: path for directory to store output
-    :type dir: str
+    :param temp_dir: path for directory to store output
+    :type temp_dir: str
     :param data: data samples
     :type data: np.matrix, shape(n_samples, n_features)
     :param ground_truth: iterator for ground truths obtained from resource reader
@@ -71,33 +64,42 @@ def _evaluate_spectral(dir, data, ground_truth, name, resolution, k_clusters, si
     :type name: str
     :param resolution: image resolution
     :type resolution: int
-    :param k_clusters: number of clusters
-    :type k_clusters: int
+    :param k_clusters: numbers of clusters
+    :type k_clusters: list(int)
     :param sim_func: can be rbf or knn
     :type sim_func: function
-    :param sim_arg: gamma in case of rbf and n_neighbours in case of knn
-    :type sim_arg: float for gamma, int for n_neighbours
+    :param sim_arg: gammas in case of rbf or n_neighbours in case of knn
+    :type sim_arg: list(float) for gamma, list(int) for n_neighbours
     :param recompute: force compute assignments and evaluation files even if they already exist.
     :type recompute: bool
     """
-    assert path.exists(dir), 'Given directory is not found'
-    assigns_file = path.join(dir, name) + '_' + str(resolution) + '.npy'
-    eval_file = path.join(dir, name) + '_' + str(resolution) + '.eval'
-    if not path.exists(assigns_file) or not path.exists(eval_file) or recompute:
-        # computing and saving assignments
-        _, assigns = spectral_clustering(data, k=k_clusters, sim_func=sim_func, sim_arg=sim_arg)
-        del _, data
-        np.save(assigns_file, assigns)
-        # computing and saving evaluations
-        f_measures = []
-        entropies = []
-        for seg, _ in ground_truth:
-            seg = seg.flatten()
-            f_measures.append(fmeasure(assigns, seg))
-            entropies.append(conditional_entropy(assigns, seg))
-        f_measures = np.asarray(f_measures)
-        entropies = np.asarray(entropies)
-        np.savetxt(eval_file, np.vstack((f_measures, entropies)))
+    for arg in sim_arg:
+        eigen_vectors = None
+        for k in k_clusters:
+            temp_dir = path.join(dir, str(k), str(sim_func).split()[1], str(arg))
+            if not path.exists(temp_dir):
+                makedirs(temp_dir)
+            assigns_file = path.join(temp_dir, name) + '_' + str(resolution) + '.npy'
+            eval_file = path.join(temp_dir, name) + '_' + str(resolution) + '.eval'
+            if not path.exists(assigns_file) or not path.exists(eval_file) or recompute:
+                if eigen_vectors is None:
+                    eigen_vectors = _spectral_clustering(data, sim_func, arg)
+                normalized_data = normalize(eigen_vectors[:, :k])
+                # computing and saving assignments
+                _, assigns = kmeans(normalized_data, 5, 0.0001, k=k)
+                del _, normalized_data
+                np.save(assigns_file, assigns)
+                # computing and saving evaluations
+                f_measures = []
+                entropies = []
+                ground_truth, gt = tee(ground_truth)
+                for seg, _ in gt:
+                    seg = seg.flatten()
+                    f_measures.append(fmeasure(assigns, seg))
+                    entropies.append(conditional_entropy(assigns, seg))
+                f_measures = np.asarray(f_measures)
+                entropies = np.asarray(entropies)
+                np.savetxt(eval_file, np.vstack((f_measures, entropies)))
 
 
 def evaluate_kmeans(dir, k_clusters, recompute=False):
@@ -125,23 +127,16 @@ def evaluate_spectral(dir, k_clusters, sim_func, sim_arg, recompute=False):
 
     :param dir: path for directory to store output
     :type dir: str
-    :param k_clusters: number of clusters
-    :type k_clusters: int
+    :param k_clusters: numbers of clusters
+    :type k_clusters: list(int)
     :param sim_func: can be rbf or knn
     :type sim_func: function
-    :param sim_arg: gamma in case of rbf and n_neighbours in case of knn
-    :type sim_arg: float for gamma, int for n_neighbours
+    :param sim_arg: gammas in case of rbf or n_neighbours in case of knn
+    :type sim_arg: list(float) for gamma, list(int) for n_neighbours
     :param recompute: force compute assignments and evaluation files even if they already exist.
     :type recompute: bool
     """
-    dir = path.join(dir, str(k_clusters), str(sim_func).split()[1], str(sim_arg))
-    if not path.exists(dir):
-        makedirs(dir)
-<<<<<<< HEAD
-    for image, ground_truth, name in islice(reader.request_data(), 0,30):
-=======
-    for image, ground_truth, name in islice(reader.request_data(), 30):
->>>>>>> bde5327d1297ce2558ae5b1520d7595fecf87902
+    for image, ground_truth, name in islice(reader.request_data(), 1):
         _evaluate_spectral(dir, image.reshape(image.shape[0] * image.shape[1], image.shape[2]), ground_truth, name,
                            image.shape[0], k_clusters, sim_func, sim_arg, recompute)
 
@@ -197,24 +192,32 @@ def read_spectral_eval(name, k, resolution, sim_func, sim_arg):
     assert path.exists(path.join(dir, name + '.eval')), 'Evaluation file is missing or not found'
     return np.load(path.join(dir, name + '.npy')), load_eval_data(path.join(dir, name + '.eval'))
 
-def big_picture_eval():
-    from visualize_data import visualize_data
-    from itertools import islice
-    from spectral_clustering import knn
-    for img, gt_iter, fname in islice(reader.request_data(),5):
-        kmeans_ouput = kmeans(img,k=5)
-        visualize_data(kmeans_ouput, gt_iter, fname)
-    for img, gt_iter, fname in islice(reader.request_data(),5):
-        visualize_data(
-            spectral_clustering(img,k=5),
-            gt_iter,
-            fname
-        )
-    for img, gt_iter,fname in islice(reader.request_data(), 5):
-        visualize_data(
-            kmeans(img, k=5),
-            spectral_clustering(img, k=5, knn, 5)
-        )
+
+# def big_picture_eval():
+#     from src.visualize_data import visualize_data
+#     from itertools import islice
+#     from src.spectral_clustering import knn
+#     for img, gt_iter, fname in islice(reader.request_data(), 5):
+#         shape = img.shape
+#         img = img.reshape((shape[0] * shape[1], shape[2]))
+#         kmeans_ouput = kmeans(img, k=5)
+#         visualize_data(kmeans_ouput[1].reshape((shape[0], shape[1])), gt_iter, fname)
+#     for img, gt_iter, fname in islice(reader.request_data(), 5):
+#         shape = img.shape
+#         img = img.reshape((shape[0] * shape[1], shape[2]))
+#         visualize_data(
+#             spectral_clustering(img, k=5)[1].reshape((shape[0], shape[1])),
+#             gt_iter,
+#             fname
+#         )
+#     for img, gt_iter,fname in islice(reader.request_data(), 5):
+#         shape = img.shape
+#         img = img.reshape((shape[0] * shape[1], shape[2]))
+#         visualize_data(
+#             kmeans(img, k=5)[1].reshape((shape[0], shape[1])),
+#             spectral_clustering(img, 5, knn, 5)[1]
+#         )
+
 
 def avg_eval(path):
     f_measures, entropies = load_eval_data(path)
@@ -255,9 +258,13 @@ def total_avg_spectral(name, resolution, sim_func, sim_arg):
 
 if __name__ == '__main__':
     environ['MKL_DYNAMIC'] = 'false'
+    counter = 0
     for k in [3, 5, 7, 9, 11]:
         evaluate_kmeans(KMEANS_DIR, k)
-        for gamma in [1, 10]:
-            evaluate_spectral(SPECTRAL_DIR, k, rbf, gamma)
-        for n_neighbours in [5]:
-            evaluate_spectral(SPECTRAL_DIR, k, knn, n_neighbours)
+    evaluate_spectral(SPECTRAL_DIR, [3, 5, 7, 9, 11], rbf, [1, 10])
+    evaluate_spectral(SPECTRAL_DIR, [3, 5, 7, 9, 11], knn, [5])
+    for k in [3, 5, 7, 9, 11]:
+        x = read_spectral_eval('100039', k, 100, rbf, 10)
+        imshow(imresize(draw_clusters(x[0], k, (100, 100)), (500, 500)))
+        print(x[1])
+        print(avg_spectral_eval('100039', k, 100, rbf, 10))
--- src/spectral_clustering.py
+++ src/spectral_clustering.py
@@ -4,6 +4,7 @@ from sklearn.neighbors import kneighbors_graph
 from scipy.sparse.csgraph import laplacian
 from sklearn.metrics.pairwise import rbf_kernel
 from src.kmeans import kmeans, draw_clusters
+from sklearn.preprocessing import normalize as normalize
 import matplotlib.pyplot as plt
 
 def knn(data, neighbours):
@@ -19,40 +20,53 @@ def normalize_rows(data):
     :return: normalized data matrix
     :rtype: numpy matrix, shape(n_samples, n_features)
     """
-    return np.divide(data, np.linalg.norm(data, axis=1)[:, None])
+    return np.divide(data, normalize(data, norm='l1'))
+
+
+def _spectral_clustering(data, sim_func, sim_arg=None, sim_mat=None):
+    """
+    Compute Normalized-cut clustering on given data samples
+
+    :param data: data samples.
+    :type data: numpy matrix, shape(n_samples, n_features)
+    :param sim_func: Similarity function used in calculating Similarity Matrix, it can be RBF or KNN.
+    :type sim_func: Function, (optional)
+    :param sim_arg: Gama parameter used im RBF Kernel or number of neighbours in N-Nearest Neighbour
+    :type sim_arg: float in RBF, int in KNN
+    :param sim_mat: similarity matrix if already computed by user
+    :type sim_mat: numpy matrix, shape(n_samples, n_samples)
+    :return: eigen Vectors
+    :rtype: numpy matrix, shape(n_clusters, n_clusters)
+    """
+    # Generating similarity matrix
+    if sim_mat is None:
+        sim_mat = sim_func(data, sim_arg)
+    # Computing laplacian matrix
+    laplace_matrix = laplacian(sim_mat, normed=False)
+    del sim_mat
+    # computing eigen vectors
+    return np.linalg.eigh(laplace_matrix)[1]
 
 
 def spectral_clustering(data, k, sim_func, sim_arg=None, sim_mat=None):
     """
-        Compute Normalized-cut clustering on given data samples
+    Compute Normalized-cut clustering on given data samples
 
-        :param data: data samples.
-        :type data: numpy matrix, shape(n_samples, n_features)
-        :param sim_func: Similarity function used in calculating Similarity Matrix, it can be RBF or KNN.
-        :type sim_func: Function, (optional)
-        :param sim_arg: Gama parameter used im RBF Kernel or number of neighbours in N-Nearest Neighbour
-        :type sim_arg: float in RBF, int in KNN
-        :param k: number of clusters
-        :type k: int
-        :param sim_mat: similarity matrix if already computed by user
-        :type sim_mat: numpy matrix, shape(n_samples, n_samples)
-        :return: centroids, samples assignments
-        :rtype: numpy matrix, shape(n_clusters, n_features), numpy matrix, shape(n_samples, 1)
-        """
-    # Generating similarity matrix
-    if sim_mat is None:
-        sim_mat = sim_func(data, sim_arg)
-        # sim_mat = np.asmatrix(np.load('rbf_sim_mat.npy'))
-    # Computing laplacian matrix
-    laplace_matrix = laplacian(sim_mat, normed=False)
-    del(sim_mat)
-    # computing eigen values and  vectors
-    eig_vectors = np.linalg.eigh(laplace_matrix)[1]
-    del(laplace_matrix)
-    # Normalizing each row
-    normalized_data = normalize_rows(eig_vectors[:, :k])
-    del(eig_vectors)
-    return kmeans(np.ma.array(normalized_data, mask=np.isnan(normalized_data)), 5, 0.0001, k=k)
+    :param data: data samples.
+    :type data: numpy matrix, shape(n_samples, n_features)
+    :param sim_func: Similarity function used in calculating Similarity Matrix, it can be RBF or KNN.
+    :type sim_func: Function, (optional)
+    :param sim_arg: Gama parameter used im RBF Kernel or number of neighbours in N-Nearest Neighbour
+    :type sim_arg: float in RBF, int in KNN
+    :param k: number of clusters
+    :type k: int
+    :param sim_mat: similarity matrix if already computed by user
+    :type sim_mat: numpy matrix, shape(n_samples, n_samples)
+    :return: centroids, samples assignments
+    :rtype: numpy matrix, shape(n_clusters, n_features), numpy matrix, shape(n_samples, 1)
+    """
+    normalized_data = normalize(_spectral_clustering(data, sim_func, sim_arg, sim_mat)[:, :k])
+    return kmeans(normalized_data, 5, 0.0001, k=k)
 
 
 def rbf(data, gamma):
